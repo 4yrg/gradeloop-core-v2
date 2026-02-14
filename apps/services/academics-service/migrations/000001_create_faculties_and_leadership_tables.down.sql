2bb62f627455fef981085991e66b3a1a5af38737
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2026-02-14 18:15:00 +0530

    chore(email): remove the go service

--- apps/services/academics-service/migrations/000001_create_faculties_and_leadership_tables.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS faculty_leadership;
-DROP TABLE IF EXISTS faculties;
--- apps/services/academics-service/migrations/000003_create_degrees_table.down.sql
+++
@@ -1,5 +0,0 @@
--- Drop degrees table
-DROP TABLE IF EXISTS degrees;
-
--- Drop ENUM type for degree level
-DROP TYPE IF EXISTS degree_level;
